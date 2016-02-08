77bdcb112be451f25a10e34b81e732a55bb20d10
Garrigos Fernando <jonafepa@gmail.com>
2016-02-08 15:54:45 +0100

    test get

--- backend/mp3-files/test/uploadTest.js
+++ backend/mp3-files/test/uploadTest.js
@@ -1,4 +1,26 @@
-var request = require("supertest"),
-    assert = require('assert'),
-    serverFileUpload = require("../serverFileUpload");
 
+var assert = require('assert'),
+    request = require("supertest"),
+    async = require("async"),
+    fileUpload = require("../serverFileUpload"),
+    logger = require("../logger");
+
+suite('GET /uploads', function(){
+  test('respond with json', function(done){
+    request(fileUpload)
+      .get('/uploads')
+      .expect(200, done);
+  })
+})
+
+// Problem with the Test
+suite('POST /api/file', function(){
+  test('respond with json', function(done){
+    request(fileUpload)
+      .post('/api/file')
+      //.send('./uploads/test.jpg')
+      //.send('./uploads/test.jpg')
+      //.attach('avatar', 'uploads/test.jpg')
+      .expect(200, done);
+  })
+})
