b986f0e462b88b000daa08ede889985cf33e4adb
Garrigos Fernando <jonafepa@gmail.com>
2016-02-10 17:11:43 +0100

    add test post

--- backend/mp3-files/test/uploadTest.js
+++ backend/mp3-files/test/uploadTest.js
@@ -4,23 +4,47 @@ var assert = require('assert'),
     async = require("async"),
     fileUpload = require("../serverFileUpload"),
     logger = require("../logger");
+    FormData = require('form-data');
 
 suite('GET /uploads', function(){
-  test('respond with json', function(done){
+  test('should return 200', function(done){
     request(fileUpload)
       .get('/uploads')
+      //.expect("Content-type",/json/)
+      .expect(200, done);
+  })
+})
+suite('GET /uploads', function(){
+ test("should return 404",function(done){
+    request(fileUpload)
+    .get("/uploads/test")
+    .expect(404,done);
+  })
+})
+
+//var form = new FormData();
+//form.append('file', '');
+
+
+suite('POST /api/file', function(){
+  test('should return 200', function(done){
+    request(fileUpload)
+      .post('/api/file')
+      //.send(form)
+      .attach('file', './uploads/test.jpg')
       .expect(200, done);
   })
 })
 
-// Problem with the Test
-/*suite('POST /api/file', function(){
-  test('respond with json', function(done){
+suite('POST /api/file', function(){
+  test('should return 404', function(done){
     request(fileUpload)
-      .post('/api/file')
-      //.send('./uploads/test.jpg')
-      //.send('./uploads/test.jpg')
-      //.attach('avatar', 'uploads/test.jpg')
-      .expect(200, done);
+      .post('/api/')
+      .send({})
+      // .attach('avatar', 'uploads/test.jpg')
+      .expect(404, done);
   })
-})*/
+})
+
+
+
